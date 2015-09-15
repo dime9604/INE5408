219607c44829a930af70745a54a53fb80a45ff24
Gustavo <dime9604@gmail.com>
2015-09-15 17:42:46 -0300

    adding documentation

--- Fila_Encadeada/FilaEnc.hpp
+++ Fila_Encadeada/FilaEnc.hpp
@@ -1,81 +1,141 @@
-#include "Elemento.hpp"
-template <typename T>
-class FilaEnc {
- private:
-	Elemento<T> *inicio;
-	Elemento<T> *fim;
-	int size;
+	/* Copyright [2015] <Gustavo Tarciso da Silva> */
+	//! Classe FilaEnc
+	/*! Classe que implementa uma estrutura em Fila com alocação dinâmica
+	na memória, cujo os dados são distribuidos de forma espalhada. Ao 
+	contrário da fila usando vetores, os dados não estão alocados em sequência,
+	portanto mecessitam de ponteiros indicando qual o próximo elemento da
+	pilha*/
 
- public:
-	FilaEnc<T>() {
-		inicio = NULL;
-		fim = NULL;
-		size = 0;
-	}
-	~FilaEnc() {
-		delete inicio;
-		size = 0;
-	}
-	void inclui(const T& dado) {
-		Elemento<T> *aux;
-		aux = new Elemento<T>(dado, NULL);
-		if (aux == NULL) {
-			throw "fila cheia";
-		} else {
-		if (filaVazia()) {
-			inicio = aux;
-		} else {
-			fim->setProximo(aux);
-		}
-		fim = aux;
-		size++;
-		}
-	}
-	T retira() {
-		Elemento<T> *aux;
-		T dado;
-		if (filaVazia()) {
-			throw "fila vazia";
-		} else {
-		aux = inicio;
-		dado = aux->getInfo();
-		inicio = aux->getProximo();
-		}
-		if (size == 1) {
+	#include "Elemento.hpp"
+	
+	template <typename T>
+	
+	class FilaEnc {
+	 private:
+		//! Um ponteiro de elemento do tipo genérico
+		/*! Um atributo que representa o inicio da fila, indicando onde
+		a fila começa*/
+		Elemento<T> *inicio;
+		//! Um ponteiro de elemento do tipo genérico
+		/*! Um atributo que representa o fim da fila, apontando o ultimo
+		elemento contido nela*/
+		Elemento<T> *fim;
+		//! Um atributo para indicar o tamanho da fila
+		/*! Um inteiro que é sempre atualizando quando um elemento é
+		adicionado ou retirado, informando o tamanho atual da lista*/
+		int size;
+
+	 public:
+		//! Construtor da classe
+		/*!
+		Construtor da classe FilaEnc. A fila é criada vazia, iniciada
+		com tamanho "0", inicio e fim vazios
+		\sa ~FilaEnc(), limparFila()
+		*/
+		FilaEnc<T>() {
+			inicio = NULL;
 			fim = NULL;
+			size = 0;
 		}
-		size--;
-		delete aux;
-		return dado;
-	}
-	T ultimo() {
-		if (filaVazia()) {
-			throw "fila vazia";
+		//! Destrutor da classe
+		/*!
+		Destrutor da classe FilaEnc. A fila é desalocada e seu tamanho
+		passa a indicar "0"
+		\sa FilaEnc(), limparFila()
+		*/
+		~FilaEnc() {
+			delete inicio;
+			size = 0;
 		}
-		return fim->getInfo();
-	}
-	T primeiro() {
-		if (filaVazia()) {
-			throw "fila vazia";
+		//! Método que adiciona um elemento no fim da fila
+		/*!
+		\param dado um elemento a ser adicionado no fim da fila
+		\sa retira()
+		*/
+		void inclui(const T& dado) {
+			Elemento<T> *aux;
+			aux = new Elemento<T>(dado, NULL);
+			if (aux == NULL) {
+				throw "fila cheia";
+			} else {
+			if (filaVazia()) {
+				inicio = aux;
+			} else {
+				fim->setProximo(aux);
+			}
+			fim = aux;
+			size++;
+			}
 		}
-		return inicio->getInfo();
-	}
-	bool filaVazia() {
-		return (size == 0);
-	}
-	void limparFila() {
-		if (filaVazia()) {
-		    throw "lista vazia";
-		} else {
-		for (int i = 0; i < size; i++) {
-		    Elemento<T> *aux;
-		    aux = inicio;
-		    inicio = aux->getProximo();
-		    size--;
-		    delete aux;
+		//! Método que retira um elemento do inicio da fila
+		/*!
+		\return dados as informações contidas no elemento removido da fila
+		\sa inclui()
+		*/
+		T retira() {
+			Elemento<T> *aux;
+			T dado;
+			if (filaVazia()) {
+				throw "fila vazia";
+			} else {
+				aux = inicio;
+				dado = aux->getInfo();
+				inicio = aux->getProximo();
+			}
+			if (size == 1) {
+			fim = NULL;
+			}
+			size--;
+			delete aux;
+			return dado;
+		}
+		//! Método que mostra as informações do ultimo elemento da fila
+		/*!
+		\return as informações do elemento do fim da fila
+		\sa primeiro()
+		*/
+		T ultimo() {
+			if (filaVazia()) {
+				throw "fila vazia";
+			}
+			return fim->getInfo();
+		}
+		//! Método que mostra as informações do primeiro elemento da fila
+		/*!
+		\return as informações do elemento no inicio da fila
+		\sa ultimo()
+		*/
+		T primeiro() {
+			if (filaVazia()) {
+				throw "fila vazia";
+			}
+			return inicio->getInfo();
+		}
+		//! Método que informa se a fila está vazia
+		/*!
+		\return um boolean informando se a lista está vazia
+		\sa FilaEnc(), ~FilaEnc(), limparFila()
+		*/
+		bool filaVazia() {
+			return (size == 0);
+		}
+		//! Método que desaloca todos os elementos da fila
+		/*!
+		\sa FilaEnc(), ~FilaEnc(), filaVazia()
+		*/
+		void limparFila() {
+			if (filaVazia()) {
+			    throw "lista vazia";
+			} else {
+				for (int i = 0; i < size; i++) {
+				    Elemento<T> *aux;
+				    aux = inicio;
+		    		inicio = aux->getProximo();
+		    		size--;
+			   		delete aux;
+		    	}
 		    }
-	    }
-	    size = 0;
-	}
-};
+		    size = 0;
+		}
+	};
 
--- Pilha_Encadeada/PilhaEnc.hpp
+++ Pilha_Encadeada/PilhaEnc.hpp
@@ -1,20 +1,50 @@
+/* Copyright [2015] <Gustavo Tarciso da Silva> */
+//! Classe PilhaEnc
+/*! Classe que implementa uma estrutura em pilha com alocação dinâmica na 
+memória, cujo os dados são distribuidos de forma espalhada. Ao contrário da
+pilha usando vetores, os dados não estão alocados em sequência, portanto 
+necessitam de ponteiros indicando qual o próximo elemento da lista*/
+
 #include "Elemento.hpp"
 template<typename T>
 
 class PilhaEnc {
  private:
+ 	//! Um ponteiro de elemento do tipo genérico
+	/*! Um atributo que representa o topo da pilha, indicando onde 
+	os elementos devem entrar e sair na pilha*/
     Elemento<T> *top;
+	//! Um atributo para indicar o tamanho da pilha
+	/*! Um inteiro que é sempre atualizado quando um elemento é adicionado
+	ou retirado, informando o tamanho atual da lista*/
     int size;
 
  public:
+ 	//! Construtor da classe
+	/*!
+	Construtor da classe PilhaEnc. A pilha é criada vazia, iniciada com tamanho
+	"0" e o topo vazio
+	\sa ~PilhaEnc(), limparPilha()
+	*/
 	PilhaEnc() {
 		top = NULL;
 		size = 0;
 	}
+	//! Destrutor da classe
+	/*!
+	Destrutor da classe PilhaEnc. A pilha é desalocada e seu tamanho passa
+	a indica "0"
+	\sa PilhaEnc(), limparPilha()
+	*/
 	~PilhaEnc() {
 		size = 0;
 		delete top;
 	}
+	//! Método que adiciona elementos no topo da pilha
+	/*!
+	\param dado um elemento a ser adicionado no topo da pilha
+	\sa desempilha(), topo()
+	*/
 	void empilha(const T& dado) {
 		Elemento<T> *aux = new Elemento<T>(dado, top);
 		if (aux != NULL) {
@@ -24,6 +54,11 @@ class PilhaEnc {
 			throw "variavel não alocada";
 		}
 	}
+	//! Método que retira elementos do topo da pilha
+	/*!
+	\return as informações do elemento retirado da pilha
+	\sa empilha(), topo()
+	*/
 	void desempilha() {
 		Elemento<T> *aux;
 		if (PilhaVazia()) {
@@ -35,17 +70,31 @@ class PilhaEnc {
 			delete aux;
 		}
 	}
+	//! Método que retorna as informações do elemento no topo da pilha
+	/*!
+	\return informações do elemento no topo da pilha
+	\sa empilha(), desempilha()
+	*/
 	T topo() {
 		if (PilhaVazia()) {
 			throw "Pilha Vazia";
 		}
 		return top->getInfo();
 	}
+	//! Método que desaloca todos os elementos da pilha
+	/*!
+	\sa PilhaEnc(), ~PilhaEnc(), PilhaVazia()
+	*/
 	void limparPilha() {
 		while (!PilhaVazia()) {
 			desempilha();
 		}
 	}
+	//! Método que verifica se a pilha está vazia
+	/*!
+	\return um boolean informando se a lista está vazia
+	\sa PilhaEnc(), ~PilhaEnc(), limparPilha()
+	*/
 	bool PilhaVazia() {
 		return (size == 0);
 	}
